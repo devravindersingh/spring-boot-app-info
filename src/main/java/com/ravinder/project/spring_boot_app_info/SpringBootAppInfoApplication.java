908a2f19d9c4b32a7e3ffc3c759f733c176cb3e1
Ravinder Singh <ravindersngh402@outlook.com>
2024-09-11 11:05:51 +0530

    intial commit

---
+++ src/main/java/com/ravinder/project/spring_boot_app_info/SpringBootAppInfoApplication.java
@@ -0,0 +1,13 @@
+package com.ravinder.project.spring_boot_app_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravinder/project/spring_boot_app_info/SpringBootAppInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravinder.project.spring_boot_app_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
